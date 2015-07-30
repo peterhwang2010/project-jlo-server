553d63f0d0d2494c008ccf19a6268023d7af73a3
Anthony Mai <amai@appnexus.com>
2015-07-30 14:01:40 -0400

    post event

--- routes/event.js
+++ routes/event.js
@@ -11,23 +11,22 @@ router.get('/event', function(request, response) {
 	var SQLquery = 'SELECT * FROM event';
 
 	var q = conn.query(SQLquery, function(error, result) {
-		response.json(result.rows)
+		response.json(result.rows);
 	});
 
 });
 
-router.get('/addevent', function(request, response) { 
+router.post('/event', function(request, response) { 
+	var obj = request.body;
 	var sql = 'INSERT INTO event (name, purpose, venue, country, start_time, end_time) VALUES ($1, $2, $3, $4, $5, $6)';
-  conn.query(sql, ['eventname', 'purpose1', 'venue1', 123, '2011-05-16 15:36:38', '2011-05-16 15:36:38'], function(error, result) {
+	 conn.query(sql, [obj.name, obj.purpose, obj.venue, obj.country, obj.start_time, obj.end_time], function(error, result) {
 		response.json(result)
 	});
 }); 
 
 router.get('/event/:id', function(request, response) { 
 
-}); 
-
-router.post('/event')
+});
 
 
 
