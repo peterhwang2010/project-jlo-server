a283c5a2711e6ba622b199f647925d307fda4d47
Anthony Mai <amai@appnexus.com>
2015-07-30 13:27:19 -0400

    simple endpoints

--- routes/event.js
+++ routes/event.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+var anyDB = require('any-db');
+var conn = anyDB.createConnection('postgres://localhost:5432/jlo');
+
 /* GET home page. */
-router.get('/event', function(req, res, next) {
-  res.send('some crap');
+router.get('/event', function(request, response) {
+
+	var SQLquery = 'SELECT * FROM event';
+
+	var q = conn.query(SQLquery, function(error, result) {
+		response.json(result.rows)
+	});
+
 });
 
-router.get('/event/:id', function(req, res) { 
+router.get('/addevent', function(request, response) { 
+	var sql = 'INSERT INTO event (name, purpose, venue, country, start_time, end_time) VALUES ($1, $2, $3, $4, $5, $6)';
+  conn.query(sql, ['eventname', 'purpose1', 'venue1', 123, '2011-05-16 15:36:38', '2011-05-16 15:36:38'], function(error, result) {
+		response.json(result)
+	});
+}); 
 
+router.get('/event/:id', function(request, response) { 
 
 }); 
 
--- schema.sql
+++ schema.sql
@@ -1,4 +1,31 @@
-CREATE TABLE test (
-	uid INTEGER PRIMARY KEY, 
-	name TEXT
+DROP TABLE event;
+CREATE TABLE event (
+	eid SERIAL PRIMARY KEY,
+	name TEXT,
+	purpose TEXT,
+	venue TEXT,
+	country INTEGER,
+	start_time TIMESTAMP,
+	end_time TIMESTAMP
+);
+
+DROP TABLE person;
+CREATE TABLE person (
+	pid SERIAL PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	email TEXT,
+	sfdc_id INTEGER,
+	job_title TEXT,
+	role_level TEXT,
+	bu TEXT
+);
+
+DROP TABLE attendance;
+CREATE TABLE attendance (
+	eid INTEGER REFERENCES event(eid),
+	pid INTEGER REFERENCES person(pid),
+	attended BOOLEAN,
+	registered BOOLEAN,
+	approved BOOLEAN
 );
